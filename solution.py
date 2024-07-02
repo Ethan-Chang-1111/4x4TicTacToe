a489aa84e34d591fb84b8724ecdceedfacb669d9
Ethan-Chang-1111 <ethan.m.chang@gmail.com>
2024-07-02 18:06:45 -0400

    checkWinner() vert, hort, diags, and corner conditions completed

--- solution.py
+++ solution.py
@@ -6,6 +6,19 @@ def main():
     # 1 represents 'X' and -1 represents 'O'
     # 0 represents an empty space
     testBoard = np.zeros((4,4))
+    addX(testBoard, (0,0))
+    # addX(testBoard, (0,1))
+    # addX(testBoard, (0,2))
+    addX(testBoard, (0,3))
+    addX(testBoard, (3,0))
+    # addX(testBoard, (1,3))
+    # addO(testBoard, (1,2))
+    addX(testBoard, (3,3))
+    # addO(testBoard, (1,1))
+    # addO(testBoard, (2,2))
+    print("Test Board:\n", testBoard)
+
+    checkWinner(testBoard)
 
 # Mutates given board to add 1 to specified location
 # Board is 4x4 np array
@@ -33,19 +46,45 @@ def add(board, loc, val):
 # True if there is a winner
 # Needs to return who the winner is
 def checkWinner(board):
+
     # Verticals
     vert = np.sum(board, axis=0)
-    print(vert)
+    # assessCondition(vert)
+    
 
     # Horizontals
+    hor = np.sum(board, axis=1)
+    # assessCondition(hor)
 
     # Diagonals
+    diagOne = board.diagonal().sum()
+    diagTwo = np.fliplr(board).diagonal().sum()
 
+    # assessCondition(diagOne)
+    # assessCondition(diagTwo)
+    
     # Corners
+    # Hard code the indicies?
+    cornerIndicies = [0, 3, 12, 15]
+    corners = np.take(board, cornerIndicies).sum()
+    # assessCondition(corners)
 
     # 2x2's
 
     return
+
+# checks if a player meets a condition
+# patterns is a list of integers
+# TODO: better define this function
+def assessCondition(pattern):
+    # X wins
+    if np.max(pattern) == 4:
+        print("X Wins")
+    
+    # O wins
+    if np.min(pattern) == -4:
+        print("O Wins")
+
 
 # checks if there are any legal moves left to play
 # returns True if there are no more moves to play
